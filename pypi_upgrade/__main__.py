1fdc28294a0f82c518085c93fbdf5bf2e64db4c8
kkristof200 <kovacskristof200@gmail.com>
2021-01-19 22:44:02 +0200

    new version

--- pypi_upgrade/__main__.py
+++ pypi_upgrade/__main__.py
@@ -1,5 +1,8 @@
+import sys
+
 def main():
     print('doing the stuff I should be doing')
+    print(sys.argv)
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="pypi_upgrade",
-    version="0.0.13",
+    version="0.0.14",
     author="Kristof",
     description="pypi_upgrade",
     long_description=long_description,
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     entry_points={
         'console_scripts': [
-            'do_main_thing=pypi_upgrade.cli:main'
+            'do_main_thing=pypi_upgrade.__main__:main'
         ]
     },
     python_requires='>=3.5',
