ca7b41e3860fc53ea55897de3d8ceec2703c07c4
kkristof200 <kovacskristof200@gmail.com>
2021-01-26 16:38:23 +0200

    Update

--- kcliutils/utils/texts/readme.py
+++ kcliutils/utils/texts/readme.py
@@ -65,12 +65,13 @@ def __create_shields(
     package_name: str,
     full_repo_name: Optional[int]
 ):
+
     return '\n\n'.join(
         [
             '\n'.join(
                 [
                     __shield('PyPI - package version', 'pypi/v/{}?logo=pypi'.format(package_name)),
-                    __shield('PyPI - license', 'pypi/l/{}'.format(package_name)),
+                    __shield('PyPI - license', 'pypi/l/{}?label=package%20license'.format(package_name)),
                     __shield('PyPI - python version', 'pypi/pyversions/{}?logo=pypi'.format(package_name)),
                     __shield('PyPI - downloads', 'pypi/dm/{}?logo=pypi'.format(package_name))
                 ]
@@ -86,9 +87,9 @@ def __create_shields(
                     __shield('GitHub - code size in bytes', 'github/languages/code-size/{}'.format(full_repo_name)),
                     __shield('GitHub - repo size', 'github/repo-size/{}'.format(full_repo_name)),
                     __shield('GitHub - lines of code', 'tokei/lines/github/{}'.format(full_repo_name)),
-                    __shield('GitHub - license', 'github/license/{}'.format(full_repo_name))
                 ]
-            )
+            ),
+            __shield('GitHub - license', 'github/license/{}?label=repo%20license'.format(full_repo_name))
         ]
     )
 
