ffce755bdb7350dd8d96e08e629f4440f126767b
kkristof200 <kovacskristof200@gmail.com>
2021-04-16 13:37:03 +0300

    Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name="kcliutils",
-    version="0.1.0",
+    version="0.1.1",
     author="Kristof",
     description="kcliutils",
     long_description=long_description,
